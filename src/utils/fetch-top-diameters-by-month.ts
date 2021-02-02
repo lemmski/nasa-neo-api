306125963e9d7bde6d120b53b89e6216bc741cfe
Aleksi Lemmetyinen <lemmetyinena@gmail.com>
2021-02-02 05:58:57 +0200

    Start writing the fetch tool

--- src/utils/fetch-top-diameters-by-month.ts
+++ src/utils/fetch-top-diameters-by-month.ts
@@ -1,1 +1,20 @@
+import progressBar from "progress-bar";
+const bar = progressBar.create(process.stdout, 60);
 console.log("Starting to fetch data to save largest objects by month");
+// Dye the bar green :) and pad percentage to a length of 3 with zeroes.
+bar.format = "\x1b[32m$bar;\x1b[m $percentage,2:0;% loaded.";
+bar.update(0.5);
+bar.update(0.6);
+console.log("");
+
+const dataStorage = {};
+
+fetchPage();
+async function fetchPage(page = 0) {
+  const response = await fetch(
+    `http://www.neowsapp.com/rest/v1/neo/browse?page=${page}&size=20&api_key=${
+      process.env.NASA_API_KEY ?? "DEMO_KEY"
+    }`
+  ).then((response) => response.json());
+  console.log("success:", response);
+}
